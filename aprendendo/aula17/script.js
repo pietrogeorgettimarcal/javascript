b195ca7562da6852b0b06edc2fb783ab380da246
Kyro Fluxo Inteligente <pietromarcal000@gmail.com>
2026-07-25 16:06:39 -0300

    commit hl-mando

--- aprendendo/aula17/script.js
+++ aprendendo/aula17/script.js
@@ -4,21 +4,20 @@ let res = document.getElementById('res')
 let valor = []
 
 function adicionar() {
-    let n = Number(num.value)
+    n = Number(num.value)
 
-    if(n >= 1 && n <= 100 && !valor.includes(n)) {
+    if(n>=1 && n<=100 && !valor.includes(n)) {
+        valor.push(n)
 
-    valor.push(n)
+        let item = document.createElement('option')
+        valor.push(item)
+        item.text = `O valor ${n} foi adicionado`
+        list.appendChild(item)
 
-    let item = document.createElement('option')
-    item.text = `O valor ${n} foi adicionado.`
-    list.appendChild(item)
-
-    res.innerHTML = ''
+        res.innerHTML = ''
     } else {
         window.alert('Número inválido ou já existente na lista.')
     }
-
     num.value = ''
     num.focus()
 }
